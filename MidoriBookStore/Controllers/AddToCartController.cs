98a57b9cca4dbac8b28612a3649ed1ab811a3542
luyendd <doduyluyen@gmail.com>
2019-03-27 13:50:21 +0700

    New Cart

---
+++ MidoriBookStore/Controllers/AddToCartController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MidoriBookStore.Controllers
+{
+    public class AddToCartController : Controller
+    {
+        // GET: AddToCart
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
